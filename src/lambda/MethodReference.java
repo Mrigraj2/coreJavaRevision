5d7e1480df4ab1b9a621b8c6496b005873cf8294
mrigraj <20201746@sbsstc.ac.in>
2025-12-29 11:31:03 +0530

    Method reference complete

--- src/lambda/MethodReference.java
+++ src/lambda/MethodReference.java
@@ -11,10 +11,25 @@ interface Addition{
 }
 
 class Dummy{
+
+    public Dummy(int x, int y){
+        System.out.println(x%y);
+    }
+
     public static void reverseString(String str){
         StringBuffer sb = new StringBuffer(str);
         sb.reverse();
         System.out.println(sb);
+    }
+
+    public void exchangeValue(int x, int y){
+        System.out.println("Before"+ "x = "+x+ "and "+ "y = "+y);
+
+        x = x^y;
+        y = x^y;
+        x = x^y;
+
+        System.out.println("After"+ "x = "+x+ "and "+ "y = "+y);
     }
 
     public void addition(int x, int y){
@@ -30,9 +45,14 @@ public class MethodReference {
         ReferenceLambda rla = Dummy::reverseString;
         rla.display("Mrigraj");
 
-        Dummy d = new Dummy();
+        Dummy d = new Dummy(4,5);
         Addition ad = d::addition;
         ad.addition(3,5);
+
+        Dummy d1 = new Dummy(4,5);
+
+        Addition rd = d1::exchangeValue;
+        rd.addition(6,7);
     }
 
 }
