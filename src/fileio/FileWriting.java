2776c45b12438735d7bed16d8169a47958a6948a
Mrigraj <20201746@sbsstc.ac.in>
2026-01-13 23:45:00 +0530

    File IO fixed

--- src/fileio/FileWriting.java
+++ src/fileio/FileWriting.java
@@ -14,11 +14,21 @@ public class FileWriting {
         {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter your keywords to write in file");
-            String s = sc.nextLine();
+
             StringBuilder paragraph = new StringBuilder();
 
-            System.out.println(s);
-            fw.write(s.getBytes());
+            while(true){
+                String s = sc.nextLine();
+                if(s.isEmpty()){
+                    break;
+                }
+                paragraph.append(s).append("\n");
+            }
+
+            String y = new String(paragraph);
+
+            System.out.println(y);
+            fw.write(y.getBytes());
         }
         catch(IOException e){
             System.out.println(e);
@@ -31,14 +41,11 @@ public class FileWriting {
             int x = fis.available();
 
             StringBuilder sb = new StringBuilder();
-
-            while(fis.read()!=-1){
-                sb.append(fis.read());
-                System.out.println(sb);
+            int ch;
+            while((ch =fis.read())!=-1){
+                fos.write(ch);
+//                System.out.println(sb);
             }
-            String s = new String(sb);
-//            System.out.println(s.getBytes());
-//            fos.write((char)s.getBytes());
 
         }
         catch(IOException e){
