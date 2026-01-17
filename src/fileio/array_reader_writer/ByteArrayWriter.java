6ef461f5fdbb0bd4950e857db001ac3ea6672e7d
Mrigraj <20201746@sbsstc.ac.in>
2026-01-18 01:21:16 +0530

    ByteArray Writer

--- src/fileio/array_reader_writer/ByteArrayWriter.java
+++ src/fileio/array_reader_writer/ByteArrayWriter.java
@@ -1,4 +1,21 @@
 package fileio.array_reader_writer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
 public class ByteArrayWriter {
+
+    public static void main(String[] args)
+    {
+        try(ByteArrayOutputStream bos = new ByteArrayOutputStream())
+        {
+            byte[] b = {'a','b','c','d','r','f','g','h','i','j'};
+            bos.write(b);
+            bos.writeTo(new FileOutputStream("text3.txt"));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
