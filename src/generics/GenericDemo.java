328709463c38fab960b7cf6dd0d2470b4011da4f
Mrigraj <20201746@sbsstc.ac.in>
2026-02-05 21:39:36 +0530

    Generic class working

--- src/generics/GenericDemo.java
+++ src/generics/GenericDemo.java
@@ -4,16 +4,20 @@ public class GenericDemo<T> {
     @SuppressWarnings("unchecked")
     T[] data = (T[]) new Object[3];
 
-    public void printArray(T[] array){
-        for(T x: array) System.out.println(x);
+    int length=0;
+
+    public void append(T v)
+    {
+        data[length++]=v;
     }
 
+    public void display() {for(T x:data) System.out.println(x);}
     public static void main(String[] args) {
         GenericDemo<String> gd = new GenericDemo<>();
-        gd.data[0] = "Hi";
-        gd.data[1] = "Hello";
-        gd.data[2] = "Maybe";
+        gd.append("Hi");
+        gd.append("Hello");
+        gd.append("Hello0000000000000000000000000");
 
-//        gd.printArray(gd.data);
+        gd.display();
     }
 }
